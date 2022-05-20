5086b0692eae999d8dfb28c2cc4e5bd6d42319ad
RoyWoods2 <han.arancibia@alumnos.duoc.cl>
2022-05-20 01:56:17 -0400

    bruces

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\RoyWoods}\Downloads\Part-08 PayPal Integration\Part-08 PayPal Integration\venv
+set VIRTUAL_ENV=C:\Users\Alumno\Integracion\MiCondominio\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
